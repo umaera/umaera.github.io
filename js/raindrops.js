edb08d79c15720a08540b903ddd8d16ece985754
UmaEra <124608386+NotYarazi@users.noreply.github.com>
2024-07-01 19:22:57 +0100

    UIv1.4

--- js/raindrops.js
+++ js/raindrops.js
@@ -2,7 +2,7 @@ function createDrop() {
     const drop = document.createElement("div");
     drop.className = "drop";
     drop.style.left = `${Math.random() * 100}vw`;
-    drop.style.animationDuration = `${Math.random() * 1 + 0.04}s`;
+    drop.style.animationDuration = `${Math.random() * 0.7 + 0.09}s`;
     document.querySelector('.rain').appendChild(drop);
 
     drop.addEventListener("animationiteration", () => {
